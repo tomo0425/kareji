c846921dc7aead0c2a0c375fe70f5d5a7ddf62eb
tomo0425 <onishi.tomoki@g.wakayama-u.jp>
2023-05-15 19:27:48 +0900

    kadai8-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,4 +12,9 @@ class PostController extends Controller
     {
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+
 }
